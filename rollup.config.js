8427a03ff1828c4b4f50ab59878c875c5e171413
Karel Maxa <karel.maxa@orchitech.cz>
2019-11-11 17:38:39 +0100

    Add babel to rollup build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/UnionReplacer.js',
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+    ],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
