eb327aca0c3a213018c6ba51b510ad96aa335436
Michal Bartoš <michal.bartos@orchitech.cz>
2019-11-21 08:02:05 +0100

    Performance tests example

---
+++ test/perf/perf.js
@@ -0,0 +1,113 @@
+(function run() {
+  const outputBox = document.getElementById('log');
+  const suites = [];
+  const benchmarkOptions = {
+    async: true,
+  };
+
+  function log(msg) {
+    outputBox.insertAdjacentHTML('beforeend', msg.replace(/\n/g, '<br>'));
+  }
+
+  function scrollToEnd() {
+    window.scroll(0, document.body.scrollHeight);
+  }
+
+  const suiteOptions = {
+    onStart: function () {
+      log('\n' + this.name + ':');
+    },
+    onCycle: function (event) {
+      log('\n' + String(event.target));
+      scrollToEnd();
+    },
+
+    onComplete: function () {
+      log('\nFastest is ' + this.filter('fastest').map('name') + '\n');
+      // Remove current suite from queue
+      suites.shift();
+      if (suites.length) {
+        // Run next suite
+        suites[0].run();
+      } else {
+        log('\nFinished.');
+      }
+      scrollToEnd();
+    },
+  };
+
+  // Expose as global
+  window.run = function () {
+    log('Testing UnionReplacer.\n');
+    suites[0].run();
+  };
+
+  /**
+   * Start of perf suites
+   */
+  (function () {
+    const htmlEscapes = [
+      [/</, '&lt;'],
+      [/>/, '&gt;'],
+      [/"/, '&quot;'],
+      [/&/, '&amp;'],
+    ];
+    const htmlEscaper = new UnionReplacer(htmlEscapes);
+    const mdHighlighter = new UnionReplacer([
+      [/^(`{3,}).*\n([\s\S]*?)(^\1`*\s*?$|\Z)/, (match, fence1, pre, fence2) => {
+        let block = `<b>${fence1}</b><br />\n`;
+        block += `<pre>${htmlEscaper.replace(pre)}</pre><br />\n`;
+        block += `<b>${fence2}</b>`;
+        return block;
+      }],
+
+      [/(^|[^`])(`+)(?!`)(.*?[^`]\2)(?!`)/, (match, lead, delim, code) => {
+        return `${htmlEscaper.replace(lead)}<code>${htmlEscaper.replace(code)}</code>`;
+      }],
+
+      [/[*~=+_-`]+/, '<b>$&</b>'],
+      [/\n/, '<br />\n'],
+    ].concat(htmlEscapes));
+
+    const toBeMarkdownHighlighted = '\
+**Markdown** code to be "highlighted"\n\
+with special care to fenced code blocks:\n\
+````\n\
+_Markdown_ within fenced code blocks is not *processed*:\n\
+```\n\
+Even embedded "fence strings" work well with **UnionEscaper**\n\
+```\n\
+````\n\
+*CommonMark is sweet & cool.*';
+
+    const mdHighlightRe = /(^(`{3,}).*\n([\s\S]*?)(^\2`*\s*?$|\Z))|((^|[^`])(`+)(?!`)(.*?[^`]\7)(?!`))|([*~=+_-`]+)|(\n)|(<)|(>)|(")|(&)/gm;
+
+    const suite = new Benchmark.Suite('First test', suiteOptions)
+      .add('UnionReplacer method', (() => {
+        mdHighlighter.replace(toBeMarkdownHighlighted);
+      }), benchmarkOptions)
+      .add('String replace method', function highlight() {
+        const myHtmlEscape = (str) => str;
+        toBeMarkdownHighlighted.replace(mdHighlightRe,
+          function (match, fenced, fence1, pre, fence2, codespan, lead, delim, code, special, nl, lt, gt, quot, amp) {
+            if (fenced) {
+              let block = `<b>${fence1}</b><br />\n`;
+              block += `<pre>${htmlEscaper.replace(pre)}</pre><br />\n`;
+              block += `<b>${fence2}</b>`;
+              return block;
+            }
+            if (codespan) {
+              return `${myHtmlEscape(lead)}<code>${myHtmlEscape.replace(code)}</code>`;
+            }
+            if (special) {
+              return `<b>${special}</b>`;
+            }
+            if (nl) {
+              return '<br />\n';
+            } // else etc.
+          });
+      }, benchmarkOptions);
+
+    suites.push(suite);
+  }());
+}());
