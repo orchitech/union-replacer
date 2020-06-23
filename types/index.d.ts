294efcad48b64d305ddb5de8f5be23bb840cc950
Martin Čížek <martin.cizek@orchitech.cz>
2020-06-23 19:57:06 +0200

    2.0.1

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-// Generated type definitions for union-replacer 2.0.0
+// Generated type definitions for union-replacer 2.0.1
 // File created by tsd-jsdoc and scripts/tsd-postprocess.js.
 // Do not modify directly.
 
